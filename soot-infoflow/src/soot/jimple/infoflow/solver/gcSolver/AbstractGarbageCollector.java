1a2ab1c48015741fe2836047893b3d9b7e4491d9
Burvenique <dimilorrain@gmail.com>
2020-02-20 13:52:43 +0100

    add leaks number in XML output

---
+++ soot-infoflow/src/soot/jimple/infoflow/solver/gcSolver/AbstractGarbageCollector.java
@@ -0,0 +1,27 @@
+package soot.jimple.infoflow.solver.gcSolver;
+
+import heros.solver.PathEdge;
+import soot.SootMethod;
+import soot.jimple.toolkits.ide.icfg.BiDiInterproceduralCFG;
+import soot.util.ConcurrentHashMultiMap;
+
+/**
+ * Abstract base class for garbage collectors
+ * 
+ * @author Steven Arzt
+ *
+ */
+public abstract class AbstractGarbageCollector<N, D> implements IGarbageCollector<N, D> {
+
+	protected final BiDiInterproceduralCFG<N, SootMethod> icfg;
+	protected final IGCReferenceProvider<D, N> referenceProvider;
+	protected final ConcurrentHashMultiMap<SootMethod, PathEdge<N, D>> jumpFunctions;
+
+	public AbstractGarbageCollector(BiDiInterproceduralCFG<N, SootMethod> icfg,
+			ConcurrentHashMultiMap<SootMethod, PathEdge<N, D>> jumpFunctions) {
+		this.icfg = icfg;
+		this.referenceProvider = new AheadOfTimeReferenceProvider<>(icfg);
+		this.jumpFunctions = jumpFunctions;
+	}
+
+}
